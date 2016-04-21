3ac5b4c29a8612a6f60a95794f35bfebd2ef2b3c
Asepps <adranepps@gmail.com>
2016-04-21 13:09:24 -0400

    made c

---
+++ Game1/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+namespace Game1
+{
+    public class Player:Entity
+    {
+        public override void LoadContent(ContentManager content, InputManager input)
+        {
+            base.LoadContent(content, input);
+        }
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+    }
+}
