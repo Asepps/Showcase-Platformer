6c6bb329fe34368a7fc3c0d2e26bf4ac8b802064
Asepps <adranepps@gmail.com>
2016-04-22 12:33:51 -0400

    Added SpriteSheetAnimation

--- Game1/SpriteSheetAnimation.cs
+++ Game1/SpriteSheetAnimation.cs
@@ -14,11 +14,35 @@ namespace Game1
        int frameCounter;
        int switchFrame;
 
+       Vector2 frames;
+       Vector2 currentFrame;
+       public Vector2 Frames
+       {
+           set { frames = value; }
+       }
+
+       public Vector2 CurrentFrame
+       {
+           set { currentFrame = value; }
+           
+       }
+       public int FrameWidth
+       {
+           get {return image.Width / (int)frames.X;}
+
+       }
+       public int FrameHeight
+       {
+           get { return image.Height / (int)frames.Y; }
+       }
+
        public override void LoadContent(ContentManager Content, Texture2D image, string text, Vector2 position)
        {
            base.LoadContent(Content, image, text, position);
            frameCounter = 0;
            switchFrame = 100;
+           frames = new Vector2(10, 9);
+           currentFrame = new Vector2(0, 0);
        }
        public override void UnloadContent()
        {
@@ -32,6 +56,12 @@ namespace Game1
                if(frameCounter >= switchFrame)
                {
                    frameCounter = 0;
+                   currentFrame.X++;
+
+                   if (currentFrame.X * FrameWidth >= image.Width)
+                       currentFrame.X = 0;
+
+                   sourceRect = new Rectangle((int)currentFrame.X * FrameWidth, (int)currentFrame.Y * FrameHeight, FrameWidth, FrameHeight);
                }
            }
            else
