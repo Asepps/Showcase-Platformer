fd554f161ea598ea2d046ecd7c9b5346986cb6a9
Asepps <adranepps@gmail.com>
2016-05-02 13:05:03 -0400

    re-commit

---
+++ Game1/NinjaStar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Game1
+{
+    class NinjaStar : GameObject
+    {
+        public NinjaStar(Texture2D texture, Vector2 velocity, Vector2 position, float rotation)
+            : base(texture)
+        {
+            this.velocity = velocity;
+            this.position = position;
+            this.rotation = rotation;
+        }
+
+        public override void Update()
+        {
+            position += velocity;
+            rotation += .2f;
+        }
+    }
+}
